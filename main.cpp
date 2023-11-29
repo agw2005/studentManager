bf5a662250db3b0f2053e8e633370d712a25b640
Danial Al Ghazali Walangadi <danialagw@students.unnes.ac.id>
2023-11-29 16:07:05 +0700

    Added a way to list students sorted by their ID, descending and ascending.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 struct student{
     std::string name;
-    long long id;
+    long long int id;
     float gpa;
 };
 
@@ -12,6 +12,44 @@ void printArray(std::string theArray[]){
     for(int i = 0; i < theArraySize ; i++){
         std::cout<<theArray[i]<<'\n';
     }
+}
+
+std::vector<student> sortVectorBiggestIDValue(std::vector<student> theVector) {
+    for (int i = 0; i < theVector.size() - 1; i++) {
+        for (int ii = i + 1; ii < theVector.size(); ii++) {
+            if (theVector[i].id < theVector[ii].id) {
+                long long int tempID = theVector[i].id;
+                std::string tempName = theVector[i].name;
+                float tempGPA = theVector[i].gpa;
+                theVector[i].id = theVector[ii].id;
+                theVector[i].name = theVector[ii].name;
+                theVector[i].gpa = theVector[ii].gpa;
+                theVector[ii].id = tempID;
+                theVector[ii].name = tempName;
+                theVector[ii].gpa = tempGPA;
+            }
+        }
+    }
+    return theVector;
+}
+
+std::vector<student> sortVectorLowestIDValue(std::vector<student> theVector) {
+    for (int i = 0; i < theVector.size() - 1; i++) {
+        for (int ii = i + 1; ii < theVector.size(); ii++) {
+            if (theVector[i].id > theVector[ii].id) {
+                long long int tempID = theVector[i].id;
+                std::string tempName = theVector[i].name;
+                float tempGPA = theVector[i].gpa;
+                theVector[i].id = theVector[ii].id;
+                theVector[i].name = theVector[ii].name;
+                theVector[i].gpa = theVector[ii].gpa;
+                theVector[ii].id = tempID;
+                theVector[ii].name = tempName;
+                theVector[ii].gpa = tempGPA;
+            }
+        }
+    }
+    return theVector;
 }
 
 std::string capitalize(std::string str) {
@@ -35,10 +73,11 @@ int main() {
             "5. Insert a grade",
             "6. Exit"
     };
-    std::vector<student> studentsVector;
+    std::vector<student> studentsVector,temporaryStudentsVector;
     student temporary;
     std::string horizontalLine = "----------------------------------------------------------------------------------------------------";
-    int startingInput, count, deleteSelection;
+    int startingInput, count, deleteSelection, sortSelection;
+    std:: string listIsRunning;
     bool idLengthIsNotTen = true;
     bool gpaValueIsNotValid = true;
     bool programIsRunning = true;
@@ -95,17 +134,56 @@ int main() {
                 std::cout<<"Student data has been deleted"<<std::endl;
                 break;
             case 3:
-                std::cout << horizontalLine << std::endl;;
+                std::cout << horizontalLine << std::endl;
                 std::cout<<"How would you like to list the students?"<<std::endl;
-                std::cout<<"1. Sorted by name\n2. Sorted by ID\n3. Sorted by GPA"<<std::endl;
-                //std::cout<<"List of students:\n";
-                //count = 1;
-                //for(student listOfStudentsIteration : studentsVector){
-                //    std::cout<<count<<". "<<listOfStudentsIteration.name<<" / "
-                //    <<listOfStudentsIteration.id<<" / "
-                //    <<listOfStudentsIteration.gpa<<'\n';
-                //    count++;
-                //}
+                std::cout<<"1. Sorted by name (descending)\n"
+                           "2. Sorted by name (ascending)\n"
+                           "3. Sorted by ID (descending)\n"
+                           "4. Sorted by ID (ascending)\n"
+                           "5. Sorted by GPA (descending)\n"
+                           "6. Sorted by GPA (ascending)"<<std::endl;
+                std::cin>>sortSelection;
+                switch(sortSelection){
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        std::cout << horizontalLine << std::endl;
+                        std::cout<<"Input: "<<sortSelection<<std::endl;
+                        std::cout << horizontalLine << std::endl;
+                        temporaryStudentsVector = sortVectorBiggestIDValue(studentsVector);
+                        count = 1;
+                        for(student i : temporaryStudentsVector){
+                            std::cout<<count<<". "<<i.id<<" / "<<i.name<<" / "<<i.gpa<<'\n';
+                            count++;
+                        }
+                        std::cout << horizontalLine << std::endl;
+                        std::cout<<"Enter any value to return to menu\n";
+                        std::cin>>listIsRunning;
+                        break;
+                    case 4:
+                        std::cout << horizontalLine << std::endl;
+                        std::cout<<"Input: "<<sortSelection<<std::endl;
+                        std::cout << horizontalLine << std::endl;
+                        temporaryStudentsVector = sortVectorLowestIDValue(studentsVector);
+                        count = 1;
+                        for(student i : temporaryStudentsVector){
+                            std::cout<<count<<". "<<i.id<<" / "<<i.name<<" / "<<i.gpa<<'\n';
+                            count++;
+                        }
+                        std::cout << horizontalLine << std::endl;
+                        std::cout<<"Enter any value to return to menu\n";
+                        std::cin>>listIsRunning;
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        std::cout << "The value you just entered is beyond our instruction, please try again.\n";
+                        break;
+                }
                 break;
             case 4:
                 break;
